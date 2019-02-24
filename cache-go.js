fb123653f680d41636ddc9d40c14d6a9e7fe6404
unstartdev <guo.yongan@outlook.com>
2019-02-24 16:58:14 +0800

    fix

--- cache-go.js
+++ cache-go.js
@@ -4,7 +4,7 @@ workbox.setConfig({
     modulePathPrefix: '/assets/workbox/'
 });
 workbox.routing.registerRoute(
-    new RegExp ('.+\.png|jpg|jpeg|svg|gif)'),
+    new RegExp ('.+\.png|jpg|jpeg|svg|gif'),
     workbox.strategies.cacheFirst()
 );
 workbox.routing.registerRoute(
