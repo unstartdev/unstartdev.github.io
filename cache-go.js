eb377165a573257ebbc324299e65e7bde03d2269
unstartdev <guo.yongan@outlook.com>
2019-03-13 12:38:52 +0800

    service worker

--- cache-go.js
+++ cache-go.js
@@ -4,51 +4,51 @@ workbox.setConfig({
 });
 workbox.routing.registerRoute(
     new RegExp('.+\.(png|jpg|jpeg|svg|gif|ico)'),
-    new workbox.strategies.cacheFirst()
+    new workbox.strategies.CacheFirst()
 );
 workbox.routing.registerRoute(
     new RegExp('https:\/\/i\.loli\.net\/(.*)'),
-    new workbox.strategies.cacheFirst({
+    new workbox.strategies.CacheFirst({
         cacheName: 'image-cache',
     })
 );
 workbox.routing.registerRoute(
     new RegExp('https:\/\/ooo\.0o0\.ooo\/(.*)'),
-    new workbox.strategies.cacheFirst({
+    new workbox.strategies.CacheFirst({
         cacheName: 'image-cache',
     })
 );
 workbox.routing.registerRoute(
     new RegExp('https:\/\/cdn\.jsdelivr\.net\/(.*)'),
-    new workbox.strategies.cacheFirst()
+    new workbox.strategies.CacheFirst()
 );
 workbox.routing.registerRoute(
     new RegExp('https:\/\/cdn1\.lncld\.net\/static\/js\/3\.0\.4\/av-min\.js'),
-    new workbox.strategies.cacheFirst()
+    new workbox.strategies.CacheFirst()
 );
 workbox.routing.registerRoute(
     new RegExp('https:\/\/cdnjs\.cloudflare\.com\/(.*)'),
-    new workbox.strategies.cacheFirst()
+    new workbox.strategies.CacheFirst()
 )
 workbox.routing.registerRoute(
     new RegExp('https:\/\/fonts\.googleapis\.com\/(.*)'),
-    new workbox.strategies.cacheFirst()
+    new workbox.strategies.CacheFirst()
 );
 workbox.routing.registerRoute(
     new RegExp('https:\/\/fonts\.gstatic\.com\/(.*)'),
-    new workbox.strategies.cacheFirst()
+    new workbox.strategies.CacheFirst()
 );
 workbox.routing.registerRoute(
     new RegExp('https:\/\/us-api\.leancloud\.cn\/(.*)'),
-    new workbox.strategies.networkOnly()
+    new workbox.strategies.NetworkOnly()
 );
 workbox.routing.registerRoute(
     new RegExp('.+\.html$'),
-    new workbox.strategies.networkFirst()
+    new workbox.strategies.NetworkFirst()
 );
 workbox.routing.registerRoute(
     new RegExp('.+loadjs\.min\.js'),
-    new workbox.strategies.cacheFirst()
+    new workbox.strategies.CacheFirst()
 );
 workbox.routing.registerRoute(
     new RegExp('.+main\.css'),
@@ -56,11 +56,11 @@ workbox.routing.registerRoute(
 );
 workbox.routing.registerRoute(
     new RegExp('https:\/\/api\.n-a\.date/(.*)'),
-    new workbox.strategies.networkOnly()
+    new workbox.strategies.NetworkOnly()
 );
 workbox.routing.registerRoute(
     new RegExp('.+cache-go\.js'),
-    new workbox.strategies.networkFirst()
+    new workbox.strategies.NetworkFirst()
 );
 self.addEventListener('install', event => {
     event.waitUntil(self.skipWaiting());
